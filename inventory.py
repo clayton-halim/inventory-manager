3eeb79d06081a2f02cce733a8010b69338cbf924
Clayton Halim <ClaytonHalim3@gmail.com>
2017-07-28 09:47:07 -0400

    Added search bar (searching not working yet)

--- inventory.py
+++ inventory.py
@@ -6,6 +6,8 @@ import random
 import numpy as np
 import re
 
+SEARCH_HINT = 'search...'
+
 class Application(object):
     def __init__(self, master):
         self.master = master
@@ -24,6 +26,22 @@ class Application(object):
         self.asset_frame.rowconfigure(1, weight=1)
         self.asset_frame.columnconfigure(0, weight=1)
         self.notebook.add(self.asset_frame, text='Asset List')
+
+        # Search bar
+        self.search_query = tk.StringVar()
+        self.search_query.set('search...')
+        self.search_bar = tk.Entry(self.asset_frame, exportselection=0, textvariable=self.search_query)
+        self.search_bar.grid(row=0, column=0,
+                                 sticky=tk.N+tk.W+tk.E)
+        self.search_bar.bind('<FocusIn>', self.search_clear)
+        self.search_query.trace('w', self.search)
+
+    def search(self, *args):
+        pass
+
+    def search_clear(self, *args):
+        if self.search_bar.get() == SEARCH_HINT:
+            self.search_query.set('')
 
 def main():
     root = tk.Tk()
