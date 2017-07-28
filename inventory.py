c535d5ef9f6dc17851dafe6187cd7bfbd1ea0f97
Clayton Halim <ClaytonHalim3@gmail.com>
2017-07-28 09:35:24 -0400

    Created gui application

---
+++ inventory.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import tkinter.font as tkFont
+import tkinter.ttk as ttk
+
+import random
+import numpy as np
+import re
+
+def main():
+    root = tk.Tk()
+    root.title('Inventory Manager')
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
