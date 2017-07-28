424290ca1818d9459c4f947af3b3ba78a2d30b6e
Clayton Halim <ClaytonHalim3@gmail.com>
2017-07-28 09:44:18 -0400

    Added notebook view and initialized frame for asset list

--- inventory.py
+++ inventory.py
@@ -6,9 +6,30 @@ import random
 import numpy as np
 import re
 
+class Application(object):
+    def __init__(self, master):
+        self.master = master
+        self.master.rowconfigure(0, weight=1)
+        self.master.columnconfigure(0, weight=1)
+
+        # Tabs for asset list / shopping cart
+        self.notebook = ttk.Notebook(self.master)
+        self.notebook.grid(row=0, column=0, sticky='nesw')
+        self.notebook.rowconfigure(0, weight=1)
+        self.notebook.columnconfigure(0, weight=1)
+
+        # Asset list frame
+        self.asset_frame = ttk.Frame(self.notebook, name='asset_frame')
+        self.asset_frame.grid(row=0, column=0, sticky='nesw')
+        self.asset_frame.rowconfigure(1, weight=1)
+        self.asset_frame.columnconfigure(0, weight=1)
+        self.notebook.add(self.asset_frame, text='Asset List')
+
 def main():
     root = tk.Tk()
+    app = Application(root)
     root.title('Inventory Manager')
+    root.update()
     root.mainloop()
 
 if __name__ == '__main__':
